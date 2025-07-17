0d5f164abef56f5c58a53524404cfecf2d4b5896
TANDONNET Josué <TANDONNETJo@slib.com>
2025-07-17 10:41:02 +0200

    Correction des erreurs 2

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from . import db, auth, blog
 
 
@@ -25,13 +25,12 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    @app.route('/')
+    def index():
+        return render_template('blog/index.html')
 
     db.init_app(app)
     app.register_blueprint(auth.bp)
     app.register_blueprint(blog.bp)
-    app.add_url_rule('/', endpoint='index')
 
     return app
